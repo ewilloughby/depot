afde2216da8c7dd1c87635d7408f25e5588891e1
Eric Willoughby <willouge@mskcc.org>
2021-03-26 16:30:45 -0400

    Add some tests for the Product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,5 +5,5 @@ class Product < ApplicationRecord
     with:   %r{\.(gif|jpg|png)\z}i,
     message:  'must be a URL for a GIF, JPG, or PNG image'
   }
-  validates price, numericality: { greater_than_or_equal_to: 0.01 }
+  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
 end

17ef6dddc2068e854df4da9b46055e9f7f7f8c33
Eric Willoughby <willouge@mskcc.org>
2021-04-03 10:43:04 -0400

    Scaffold Cart

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_26_010751) do
+ActiveRecord::Schema.define(version: 2021_04_03_144139) do
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "title"
